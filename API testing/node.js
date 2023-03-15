ab50366d94d5e6160c941972b4d9b440bf85fe8c
eladjmc <eladjmc88@gmail.com>
2023-03-15 15:16:28 +0200

    added api testing

--- API testing/node.js
+++ API testing/node.js
@@ -1,56 +1,32 @@
 const http = require('http');
-const fs = require('fs');
+const axios = require('axios');
 
-const getNewInfo = async (url) => {
-  
-}
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'GET') {
-    if (req.url === '/raw-html') {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write('<h1>Welcome - Elads Raw html print</h1>');
-      res.end();
-    } else if (req.url === '/users') {
-      fs.readFile('./users/users.json', (err, data) => {
-        if (err) throw err;
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.write(data);
-        res.end();
+  if (req.method === 'GET' && req.url === '/cats') {
+    const options = {
+      url: 'https://catfact.ninja/fact',
+      method: 'GET'
+    };
+
+    axios(options)
+      .then(response => {
+        const fact = response.data;
+        const length = fact.length;
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`The Cats fact is ${length} characters long`);
+      })
+      .catch(error => {
+        console.error(error);
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('An error occurred while retrieving the cats fact');
       });
-    } else if (req.url === '/') {
-      fs.readFile('index.html', (err, data) => {
-        if (err) throw err;
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        res.end();
-      });
-    } else if (req.url === './index.css') {
-      fs.readFile('index.css', (err, data) => {
-        if (err) throw err;
-        res.writeHead(200, { 'Content-Type': 'text/css' });
-        res.write(data);
-        res.end();
-      });
-    } else if (req.url === '/index.js') {
-      fs.readFile('index.js', (err, data) => {
-        if (err) throw err;
-        res.writeHead(200, { 'Content-Type': 'text/javascript' });
-        res.write(data);
-        res.end();
-      });
-    } else {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.write('404 Not Found');
-      res.end();
-    }
   } else {
-    res.writeHead(405, { 'Content-Type': 'text/plain' });
-    res.write('405 Method Not Allowed');
-    res.end();
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
   }
 });
 
 server.listen(3000, () => {
-  console.log('Server running on port 3000');
+  console.log('Server listening on port 3000');
 });
